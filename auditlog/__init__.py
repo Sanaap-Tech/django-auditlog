ebd7323d3bde8907affc61fdf8aa10cf8b122f32
Mostafa Eftekhari <mostafa.eftekhari@gmail.com>
2025-10-01 07:59:25 +0330

    raise exception on LogEntry model import error

--- auditlog/__init__.py
+++ auditlog/__init__.py
@@ -1,6 +1,7 @@
 from importlib.metadata import version
 from django.apps import apps as django_apps
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 __version__ = version("django-auditlog")
 
@@ -8,5 +9,12 @@ __version__ = version("django-auditlog")
 def get_logentry_model():
     try:
         return django_apps.get_model(settings.AUDITLOG_LOGENTRY_MODEL, require_ready=False)
-    except:
-        pass
+    except ValueError:
+        raise ImproperlyConfigured(
+            "AUDITLOG_ENTRY_MODEL must be of the form 'app_label.model_name'"
+        )
+    except LookupError:
+        raise ImproperlyConfigured(
+            "AUDITLOG_LOGENTRY_MODEL refers to model '%s' that has not been installed"
+            % settings.AUDITLOG_LOGENTRY_MODEL
+        )
